72025d3a2db563b25068c822fc45fb236ed69fd5
chanchaldas71 <abhijitdas47514354@gmail.com>
2024-03-30 02:48:40 +0600

    added

--- function.js
+++ function.js
@@ -1,4 +1,7 @@
 function square (num1) {
     console.log(num1);
+    console.log('value of the number parameter', num1)
+    const borgo = num1 * num1;
+    console.log('square of t he given number is',borgo)
 }
-square();
+square(55);
