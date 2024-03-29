9021342d9e1f1e6f63f320057483e72c44037d54
chanchaldas71 <abhijitdas47514354@gmail.com>
2024-03-30 03:48:52 +0600

    aded

--- arguments.js
+++ arguments.js
@@ -1,3 +1,18 @@
 function doubleIt (number) {
-    const doubled = Number *
+    const doubled = Number * 2;
+    console.log(number, doubled);
 }
+console.log('I will call the function')
+doubleIt(15);
+console.log('----------------------')
+doubleIt(88);
+console.log('-----------------------')
+doubleIt(873);
+
+function difference (num1, num2) {
+    const diff = num1 - num2;
+    console.log(num1, num2, 'difference is', diff);
+}
+const fatherAge = 40;
+const myAge = 10;
+difference(fatherAge, myAge);
