7e4bc174f98a1edf40e7ef8d1d01c079a72f2d3d
chanchaldas71 <abhijitdas47514354@gmail.com>
2024-03-30 02:50:59 +0600

    added

--- function.js
+++ function.js
@@ -5,3 +5,15 @@ function square (num1) {
     console.log('square of t he given number is',borgo)
 }
 square(55);
+square(5);
+square(550);
+square(55000);
+square(5501);
+square(5504);
+square(03);
+
+function add (num1, num2) {
+    const sum = num1 + num2;
+    console.log(sum);
+}
+add(11,11);
